f7a12e1d6ea64e4a39756b7c71d57a15eeb40ad0
pentran <penny.tran@stonybrook.edu>
2023-05-07 20:48:56 +0000

    added a data folder

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import pandas as pd
+
+df = pd.read_csv('./data/diagnoses2019.csv')
+
+app = Flask(__name__)
+
+@app.route('/', method="GET")
+def home():
+    return 'this is a API service for MN ICD code details'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/icd/<value>', methods=['GET'])
+def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['principal_diagnosis_code'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else:
+        return filtered.to_json(orient="records")
+
+@app.route('icd/<value>/sex/<value2>')
+def icdcode2(value, value2):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered2 = filtered[filtered['principal_diagnosis_code'] == value]
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
